4c30ce3cf7cb717a3fe0076a551c6bc6ff038829
Mn0ky <67206766+Mn0ky@users.noreply.github.com>
2021-11-24 18:56:43 -0500

    socket additions

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-let app = require("express")();
-let http = require("http").Server(app);
+const express = require('express');
+const app = require("express")();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 
 app.get("/", function (req, res) {
@@ -7,9 +11,15 @@ app.get("/", function (req, res) {
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, function () {
+server.listen(PORT, function () {
     console.log("listening on localhost:3000");
 });
+
+io.on("connection",
+    function(socket) {
+        console.log("A user connected NOW", socket.id);
+    });
+
 
 // Console will print the message
 console.log('Server running!!');
