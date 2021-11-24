44353db978c9326457625e1365cde97fea7e66cd
Mn0ky <67206766+Mn0ky@users.noreply.github.com>
2021-11-24 17:00:33 -0500

    steamapi

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-var http = require("http");
+let app = require("express")();
+let http = require("http").Server(app);
+
+
+app.get("/", function (req, res) {
+    res.sendfile("index.html");
+});
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, function () {
@@ -7,3 +13,4 @@ http.listen(PORT, function () {
 
 // Console will print the message
 console.log('Server running!!');
+// console.log(app.use(express.static(__dirname + '/public')));
